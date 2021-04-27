50468339bac5fccdabec0ba8b7efa6447484d064
Mékéné <64429720+IsmaelMekene@users.noreply.github.com>
2021-04-27 09:17:21 +0200

    Create preprocessing.py

---
+++ data/preprocessing.py
@@ -0,0 +1,87 @@
+
+
+#Load the dataset 
+dataset = tf.keras.utils.get_file(
+    "modelnet.zip",
+    "http://3dvision.princeton.edu/projects/2014/3DShapeNets/ModelNet10.zip",
+    extract=True,
+)
+dataset = os.path.join(os.path.dirname(dataset), "ModelNet10")
+
+
+#Viz
+mesh = trimesh.load(os.path.join(dataset, "bed/train/bed_0019.off"))
+mesh.show()
+
+
+#Sample Mesh
+points = mesh.sample(4096)#(3000)#(2048)
+
+fig = plt.figure(figsize=(5, 5))
+ax = fig.add_subplot(111, projection="3d")
+ax.scatter(points[:, 0], points[:, 1], points[:, 2])
+#ax.set_axis_off()
+plt.show()
+
+
+
+#Parse Dataset 
+def parse_dataset(num_points=4096):
+
+    train_points = []
+    train_labels = []
+    test_points = []
+    test_labels = []
+    class_map = {}
+    folders = glob.glob(os.path.join(dataset, "[!README]*"))
+
+    for i, folder in enumerate(folders):
+        print("processing class: {}".format(os.path.basename(folder)))
+        # store folder name with ID so we can retrieve later
+        class_map[i] = folder.split("/")[-1]
+        # gather all files
+        train_files = glob.glob(os.path.join(folder, "train/*"))
+        test_files = glob.glob(os.path.join(folder, "test/*"))
+
+        for f in train_files:
+            train_points.append(trimesh.load(f).sample(num_points))
+            train_labels.append(i)
+
+        for f in test_files:
+            test_points.append(trimesh.load(f).sample(num_points))
+            test_labels.append(i)
+
+    return (
+        np.array(train_points),
+        np.array(test_points),
+        np.array(train_labels),
+        np.array(test_labels),
+        class_map,
+    )
+  
+  
+  
+#process the parser
+NUMBER_POINTS = 4096
+NUMBER_CLASSES = 10
+BATCH_SIZE = 32
+
+train_points, test_points, train_labels, test_labels, CLASS_MAP = parse_dataset(NUMBER_POINTS)
+
+
+#Data Augmentation
+def augmentation(points, label):
+    # jitter points
+    points += tf.random.uniform(points.shape, -0.005, 0.005, dtype=tf.float64)
+    # shuffle points
+    points = tf.random.shuffle(points)
+    return points, label
+  
+  
+train_dataset = tf.data.Dataset.from_tensor_slices((train_points, train_labels))
+test_dataset = tf.data.Dataset.from_tensor_slices((test_points, test_labels))
+
+train_dataset = train_dataset.shuffle(len(train_points)).map(augmentation).batch(BATCH_SIZE)
+test_dataset = test_dataset.shuffle(len(test_points)).batch(BATCH_SIZE)
+
+
