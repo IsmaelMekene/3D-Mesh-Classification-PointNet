09e2438be48ecec1d3af06ea5903449719803cd8
Mékéné <64429720+IsmaelMekene@users.noreply.github.com>
2021-04-27 09:18:18 +0200

    Create inference.py

---
+++ inference.py
@@ -0,0 +1,28 @@
+
+
+
+
+data = test_dataset.take(15)
+
+points, labels = list(data)[0]
+points = points[:8, ...]
+labels = labels[:8, ...]
+
+# run test data through model
+preds = model.predict(points)
+preds = tf.math.argmax(preds, -1)
+
+points = points.numpy()
+
+# plot points with predicted class and label
+fig = plt.figure(figsize=(15, 10))
+for i in range(8):
+    ax = fig.add_subplot(2, 4, i + 1, projection="3d")
+    ax.scatter(points[i, :, 0], points[i, :, 1], points[i, :, 2])
+    ax.set_title(
+        "Pred: {:}, True: {:}".format(
+            CLASS_MAP[preds[i].numpy()], CLASS_MAP[labels.numpy()[i]]
+        )
+    )
+    #ax.set_axis_off()
+plt.show()
